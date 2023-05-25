3113e78c2bfe71dec7d71c34c739081f93387202
Leonardo Abreu <superleor30@hotmail.com>
2023-05-25 16:51:57 -0300

    feat: added a chart selector

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from scripts.teste1 import *
 from pysus.online_data import sinasc, SINAN
 
 app = Flask(__name__)
-
-#df = sinasc.download('PI', [2012, 2014])
 
 uf = 'PI'
 disease1 = 'Sífilis Congênita'
@@ -16,20 +14,21 @@ df_sinasc = download_sisinfo('SINASC', uf, ano_ini, ano_fim)
 df_sinan_sif_cong = download_sisinfo('SINAN', disease1, ano_ini, ano_fim)
 df_sinan_sif_gest = download_sisinfo('SINAN', disease2, ano_ini, ano_fim)
 
-# descrever o problema de uniformidade de base de dados
-# focar em escrita
-# 
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    media_pesos_por_mes = calculo_media_peso2(df_sinasc)
-    chart1 = create_bar_chart(media_pesos_por_mes)
+    chart_type = request.args.get('chart_type', default='chart1')
 
-    taxa_sifilis_congenita = calcular_taxa_sifilis_congenita2(df_sinasc, df_sinan_sif_cong, df_sinan_sif_gest)
-    chart2 = create_line_chart(taxa_sifilis_congenita)
+    if chart_type == 'chart1':
+        media_pesos_por_mes = calculo_media_peso2(df_sinasc)
+        chart = create_bar_chart(media_pesos_por_mes)
+    elif chart_type == 'chart2':
+        taxa_sifilis_congenita = calcular_taxa_sifilis_congenita2(df_sinasc, df_sinan_sif_cong, df_sinan_sif_gest)
+        chart = create_line_chart(taxa_sifilis_congenita)
+    else:
+        chart = None
 
-    # Render the template with the results and the charts
-    return render_template("index.html", chart1=chart1, chart2=chart2)
+    # Render the template with the selected chart
+    return render_template("index.html", chart=chart)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
